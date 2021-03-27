02eb2d90a4b1f6b9f8e59d46c25ff9a3088a509c
eddysims <eddy.s@getjobber.com>
2021-03-26 20:33:32 -0600

    update lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  "*.{js,jsx,ts,tsx}": ["yarn lint", "jest --bail --findRelatedTests"],
+  "*.{js,jsx,ts,tsx}": ["yarn lint:js", "jest --bail --findRelatedTests"],
   "*.{md,mdx}": ["yarn lint:md"],
 };
