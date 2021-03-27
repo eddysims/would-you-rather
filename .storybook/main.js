c31d2932192709786c244ed39d6df202a12dbdf3
eddysims <eddy.s@getjobber.com>
2021-03-26 19:55:32 -0600

    adds Navigation component

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const { TsconfigPathsPlugin } = require("tsconfig-paths-webpack-plugin");
 
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
@@ -7,4 +8,10 @@ module.exports = {
     "@storybook/addon-essentials",
     "storybook-css-modules-preset",
   ],
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      new TsconfigPathsPlugin({ extensions: config.resolve.extensions }),
+    ];
+    return config;
+  },
 };
---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,28 @@
+import { NavigationLink } from "@/components/NavigationLink";
+
+import styles from "./Navigation.module.css";
+
+export function Navigation() {
+  const navItems = [
+    {
+      to: "/signin",
+      title: "Sign In",
+    },
+    {
+      to: "/signin",
+      title: "Submit a question",
+    },
+  ];
+
+  return (
+    <nav className={styles.navigation}>
+      <ul className={styles.list}>
+        {navItems.map((item) => (
+          <li className={styles.item}>
+            <NavigationLink to={item.to} title={item.title} />
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
