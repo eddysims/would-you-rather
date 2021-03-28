2214ed40213f60108df096c6a09231804386f351
eddysims <eddy.s@getjobber.com>
2021-03-27 23:32:15 -0600

    adds Layout to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { Navigation } from "@/components/Navigation";
+import { Layout } from "@/components/Layout";
 import { Logo } from "@/components/Logo";
 import { Button } from "@/components/Button";
 import { signIn, signOut, useSession } from "next-auth/client";
@@ -15,16 +15,20 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navigation />
-      <div className={styles.logoContainer}>
-        <Logo size="large" />
-      </div>
-      {session && JSON.stringify(session.user.name)}
-      {session ? (
-        <Button onClick={signOut} title="Sign out" />
-      ) : (
-        <Button onClick={() => signIn("github")} title="Sign in with github" />
-      )}
+      <Layout>
+        <div className={styles.logoContainer}>
+          <Logo size="large" />
+        </div>
+        {session && JSON.stringify(session.user.name)}
+        {session ? (
+          <Button onClick={signOut} title="Sign out" />
+        ) : (
+          <Button
+            onClick={() => signIn("github")}
+            title="Sign in with github"
+          />
+        )}
+      </Layout>
     </>
   );
 }
