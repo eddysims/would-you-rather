13ffa5c4b52fb31642b18504e09164f42c0a1cf6
eddysims <eddy.s@getjobber.com>
2021-03-29 21:09:40 -0600

    adds user on signin

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,22 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
+import { gql } from "@apollo/client";
+import { client } from "@/lib/apollo";
+
+const ADD_USER_MUTATION = gql`
+  mutation addUser(
+    $id: String!
+    $name: String!
+    $avatar: String
+    $provider: String!
+  ) {
+    insert_users_one(
+      object: { id: $id, name: $name, avatarUrl: $avatar, provider: $provider }
+    ) {
+      id
+    }
+  }
+`;
 
 export default NextAuth({
   providers: [
@@ -13,4 +30,52 @@ export default NextAuth({
     }),
   ],
   secret: process.env.SECRET,
+  session: {
+    jwt: true,
+  },
+  callbacks: {
+    async session(session, token) {
+      const callbackSession = {
+        ...session,
+      };
+
+      if (token.provider) {
+        callbackSession.user.provider = token.provider;
+      }
+
+      if (token.sub) {
+        callbackSession.user.id = token.sub;
+      }
+
+      return session;
+    },
+    async jwt(token, user, account) {
+      const userIsSignedIn = user && account;
+
+      const callbackToken = {
+        ...token,
+      };
+
+      if (account) {
+        callbackToken.provider = account.provider;
+      }
+
+      /**
+       * Create the user in the DB
+       */
+      if (userIsSignedIn) {
+        client.mutate({
+          mutation: ADD_USER_MUTATION,
+          variables: {
+            id: `${user.id}`,
+            name: token.name,
+            provider: account.provider,
+            avatar: token.picture,
+          },
+        });
+      }
+
+      return callbackToken;
+    },
+  },
 });
