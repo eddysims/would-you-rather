08a9b9d7897375183f6e16a41bc591b4adbc8231
eddysims <eddy.s@getjobber.com>
2021-03-30 12:22:25 -0600

    adds an Avatar component

---
+++ src/components/Avatar/Avatar.test.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import { Avatar } from ".";
+
+afterEach(cleanup);
+
+it("renders an Icon by default", () => {
+  const { getByTestId } = render(<Avatar />);
+  expect(getByTestId("icon-User")).toBeInstanceOf(SVGElement);
+});
+
+it("renders an aria-label by default", () => {
+  const { getByLabelText } = render(<Avatar />);
+  expect(getByLabelText("Current users avatar")).toBeInstanceOf(HTMLDivElement);
+});
+
+it("renders an aria-label with name when set", () => {
+  const { getByLabelText } = render(<Avatar name="Eddy Sims" />);
+  expect(getByLabelText("Avatar for user Eddy Sims")).toBeInstanceOf(
+    HTMLDivElement
+  );
+});
+
+it("only renders 1 initial with short name", () => {
+  const { getByText } = render(<Avatar name="Eddy" />);
+  expect(getByText("E")).toBeInstanceOf(HTMLDivElement);
+});
+
+it("renders 2 initial with normal name", () => {
+  const { getByText } = render(<Avatar name="Eddy Sims" />);
+  expect(getByText("ES")).toBeInstanceOf(HTMLDivElement);
+});
+
+it("renders first and last initial with longer name", () => {
+  const { getByText } = render(<Avatar name="Eddy Darell Sims" />);
+  expect(getByText("ES")).toBeInstanceOf(HTMLDivElement);
+});
+
+it("does not render name if imageUrl is set", () => {
+  const { queryByText } = render(<Avatar imageUrl="foo" name="Eddy Sims" />);
+  expect(queryByText("ES")).toBeNull();
+});
---
+++ src/components/Avatar/Avatar.tsx
@@ -0,0 +1,48 @@
+import { Icon } from "@/components/Icon";
+import styles from "./Avatar.module.css";
+
+interface AvatarProps {
+  /**
+   * The name of the user that is being displayed. This name will also be used
+   * to generate the iniials that show for the Avatar
+   */
+  readonly name?: string;
+  /**
+   * The url to the image that will be displayed
+   */
+  readonly imageUrl?: string;
+}
+
+export function Avatar({ name, imageUrl }: AvatarProps) {
+  const initials = getInitials();
+  const style = {
+    backgroundImage: `url(${imageUrl})`,
+  };
+
+  return (
+    <div
+      className={styles.avatar}
+      style={style}
+      aria-label={name ? `Avatar for user ${name}` : "Current users avatar"}
+    >
+      {!imageUrl && (name ? initials : <Icon icon="User" />)}
+    </div>
+  );
+
+  function getInitials() {
+    if (!name) {
+      return undefined;
+    }
+
+    const initialsArr = name
+      .split(" ")
+      .map((item) => (item.length > 0 ? item[0] : false))
+      .filter((item) => item !== false);
+
+    if (initialsArr.length > 1) {
+      return `${initialsArr[0]}${initialsArr.pop()}`;
+    }
+
+    return initialsArr[0];
+  }
+}
